bd9da56b0b259160e14d8cc4d4613d87b9e8c265
Anastasia Aleksashkina <aleksashkina.nastya@yandex.ru>
2022-12-06 23:45:49 +0300

    feat: отправить компонент Profile

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,4 +1,6 @@
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import SearchForm from "../SearchForm/SearchForm";
 import "./SavedMovies.css";
 
@@ -7,6 +9,9 @@ function SavedMovies() {
     <>
       <Header />
       <SearchForm />
+      <MoviesCardList />
+      <div className="saved-movies__interval"></div>
+      <Footer />
     </>
   );
 }
