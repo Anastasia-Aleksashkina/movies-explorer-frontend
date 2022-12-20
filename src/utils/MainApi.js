2d589b33ab7cb2ca2a41d57fcbe79d5fcb8d8fe8
Anastasia Aleksashkina <aleksashkina.nastya@yandex.ru>
2022-12-20 22:10:43 +0300

    feat: отправить MainApi и MoviesApi

---
+++ src/utils/MainApi.js
@@ -0,0 +1,50 @@
+export const BASE_URL = "https://api.aleksashkina.movies.nomoredomains.club";
+
+function request({ url, method = "POST", data, token }) {
+  return fetch(`${BASE_URL}${url}`, {
+    credentials: "include",
+    mode: "cors",
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      ...(!!token && { Authorization: `Bearer ${token}` }),
+    },
+    ...(!!data && { body: JSON.stringify(data) }),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
+}
+
+// Регистрация
+export const register = (name, email, password) => {
+  return request({
+    url: "/signup",
+    data: { name, email, password },
+  });
+};
+
+// Авторизация
+export const authorization = (email, password) => {
+  return request({
+    url: "/signin",
+    data: { email, password },
+  });
+};
+
+// Проверка токена и получение данных пользователя
+export const getContent = (token) => {
+  return request({
+    url: "/users/me",
+    method: "GET",
+    token,
+  });
+};
+
+export const logout = () => {
+  return request({
+    url: "/logout",
+  });
+};
